1f6519a0c9f4a2223a4ccffb3363d8ee29d20cd1
Fletcher Nichol <fnichol@nichol.ca>
2011-12-12 20:16:44 -0700

    Rough implementation of per-user rbenv installs.

--- recipes/user_install.rb
+++ recipes/user_install.rb
@@ -17,3 +17,21 @@
 # limitations under the License.
 #
 
+include_recipe 'rbenv'
+
+install_rbenv_pkg_prereqs
+
+Array(node['rbenv']['user_installs']).each do |rb_user|
+  upgrade_strategy  = build_upgrade_strategy(rb_user['upgrade'])
+  git_url           = rb_user['git_url'] || node['rbenv']['git_url']
+  git_ref           = rb_user['git_ref'] || node['rbenv']['git_ref']
+  rbenv_prefix      = rb_user['home'] || ::File.join(
+    node['rbenv']['user_home_root'], rb_user['user'], '.rbenv')
+
+  install_or_upgrade_rbenv  :rbenv_prefix => rbenv_prefix,
+                            :git_url => git_url,
+                            :git_ref => git_ref,
+                            :upgrade_strategy => upgrade_strategy,
+                            :user => rb_user['user'],
+                            :group => rb_user['group']
+end
