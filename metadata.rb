6b30a6e242e38ebf463bbd1ed18fc40824af2100
Fletcher Nichol <fnichol@nichol.ca>
2011-12-21 12:08:44 -0700

    Update metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,11 @@ maintainer       "Fletcher Nichol"
 maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Manages rbenv and its installed rubies. Several LWRPs are also defined."
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description "Please refer to README.md (it's long)."
 version          "0.6.0"
+
+# if using the rbenv_ruby LWRP, ruby-build must be installed
+recommends "ruby_build"
+
+# if using jruby, java is required on system
+recommends "java"
