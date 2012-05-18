f40acd28d7cc7f22eea35ae36ebb62a829d15965
Fletcher Nichol <fnichol@nichol.ca>
2012-05-18 16:22:27 -0600

    Up version for development.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Manages rbenv and its installed rubies. Several LWRPs are also defined."
 long_description "Please refer to README.md (it's long)."
-version          "0.6.10"
+version          "0.6.11"
 
 recommends "ruby_build"       # if using the rbenv_ruby LWRP, ruby-build must be installed
 recommends "java", "> 1.4.0"  # if using jruby, java is required on system
