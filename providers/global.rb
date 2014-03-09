0a3018634bafe58ad21c6ee271af015220e444b9
Fletcher Nichol <fnichol@nichol.ca>
2014-03-08 17:53:28 -0700

    Refactor rbenv_global provider and add specs.

--- providers/global.rb
+++ providers/global.rb
@@ -4,7 +4,7 @@
 #
 # Author:: Fletcher Nichol <fnichol@nichol.ca>
 #
-# Copyright 2011, Fletcher Nichol
+# Copyright 2011, 2014 Fletcher Nichol
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,22 +19,33 @@
 # limitations under the License.
 #
 
+def whyrun_supported?
+  true
+end
+
+use_inline_resources
+
 include Chef::Rbenv::ScriptHelpers
 
 action :create do
-  if current_global_version != new_resource.rbenv_version
-    command = %{rbenv global #{new_resource.rbenv_version}}
-
-    rbenv_script "#{command} #{which_rbenv}" do
-      code        command
-      user        new_resource.user       if new_resource.user
-      root_path   new_resource.root_path  if new_resource.root_path
-
-      action      :nothing
-    end.run_action(:run)
-
-    new_resource.updated_by_last_action(true)
+  if current_global_version_correct?
+    set_updated { run_script }
   else
     Chef::Log.debug("#{new_resource} is already set - nothing to do")
   end
+end
+
+def run_script
+  command = %{rbenv global #{new_resource.rbenv_version}}
+
+  rbenv_script "#{command} #{which_rbenv}" do
+    code        command
+    user        new_resource.user       if new_resource.user
+    root_path   new_resource.root_path  if new_resource.root_path
+    action      :run
+  end
+end
+
+def current_global_version_correct?
+  current_global_version != new_resource.rbenv_version
 end
