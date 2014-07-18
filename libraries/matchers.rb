ce9c1494346d261e0091326e963e1ffc7a5d6e92
Alan Moran & Leandro Cacciagioni <pair+bonzofenix+lcacciagioni@wmg.com>
2014-07-18 17:09:41 +0000

    add matchers for rbenv_gem and rbenv_ruby

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -6,4 +6,12 @@ if defined?(ChefSpec)
   def run_rbenv_script(name)
     ChefSpec::Matchers::ResourceMatcher.new(:rbenv_script, :run, name)
   end
+
+  def install_rbenv_gem(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:rbenv_gem, :install, name)
+  end
+
+  def install_rbenv_ruby(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:rbenv_ruby, :install, name)
+  end
 end
