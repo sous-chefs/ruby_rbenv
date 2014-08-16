cc4e15a3200ae44a7ed989a51b9c796d7a42baf2
Umang Chouhan <uchouhan@optimiscorp.com>
2014-08-17 02:18:34 +0530

    add matcher for rbenv gem install

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,4 +1,8 @@
 if defined?(ChefSpec)
+  def install_rbenv_gem(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:rbenv_gem, :install, name)
+  end
+  
   def install_rbenv_plugin(name)
     ChefSpec::Matchers::ResourceMatcher.new(:rbenv_plugin, :install, name)
   end
