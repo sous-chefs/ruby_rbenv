bf27ae7998708f8389d6dde48da31efe803d695e
Daniel O'Connor <daniel.oconnor@gmail.com>
2015-07-27 14:25:34 +0930

    Rubocop

--- providers/script.rb
+++ providers/script.rb
@@ -32,19 +32,20 @@ action :run do
 end
 
 def run_script
-  script_code         = build_script_code
-  script_environment  = build_script_environment
+  script_code        = build_script_code
+  script_environment = build_script_environment
 
   script new_resource.name do
-    interpreter   'bash'
-    code          script_code
-    user          new_resource.user     if new_resource.user
-    creates       new_resource.creates  if new_resource.creates
-    cwd           new_resource.cwd      if new_resource.cwd
-    group         new_resource.group    if new_resource.group
-    returns       new_resource.returns  if new_resource.returns
-    timeout       new_resource.timeout  if new_resource.timeout
-    umask         new_resource.umask    if new_resource.umask
+    interpreter 'bash'
+    code        script_code
+    user        new_resource.user     if new_resource.user
+    creates     new_resource.creates  if new_resource.creates
+    cwd         new_resource.cwd      if new_resource.cwd
+    group       new_resource.group    if new_resource.group
+    returns     new_resource.returns  if new_resource.returns
+    timeout     new_resource.timeout  if new_resource.timeout
+    umask       new_resource.umask    if new_resource.umask
+
     environment(script_environment)
   end
 end
@@ -63,12 +64,11 @@ end
 
 def build_script_environment
   script_env = { 'RBENV_ROOT' => rbenv_root }
-  if new_resource.environment
-    script_env.merge!(new_resource.environment)
-  end
 
+  script_env.merge!(new_resource.environment) if new_resource.environment
+  
   if new_resource.path
-    script_env.merge!({ 'PATH' =>  "#{new_resource.path.join(':')}:#{ENV["PATH"]}" })
+    script_env.merge!('PATH' => "#{new_resource.path.join(':')}:#{ENV['PATH']}")
   end
 
   if new_resource.user
