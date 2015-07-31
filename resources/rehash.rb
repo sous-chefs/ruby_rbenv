680b4d742e03ccd27eeeb224971b6df1cb6fd5a3
Tim Smith <tim@cozy.co>
2015-07-30 22:52:37 -0700

    Use default action introduced in Chef 0.10.10

--- resources/rehash.rb
+++ resources/rehash.rb
@@ -20,12 +20,8 @@
 #
 
 actions :run
+default_action :run
 
 attribute :name,      kind_of: String, name_attribute: true
 attribute :user,      kind_of: String
 attribute :root_path, kind_of: String
-
-def initialize(*args)
-  super
-  @action = :run
-end
