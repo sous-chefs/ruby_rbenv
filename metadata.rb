e8762a3ea2fa1fbdc8011e75b7beb65f905167c1
Dan Webb <dan.webb@damacus.io>
2017-03-01 00:26:31 +0000

    Fix lack of respond to

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,13 @@
 name 'ruby_rbenv'
 maintainer 'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
-issues_url 'https://github.com/sous-chefs/ruby_rbenv/issues'
-source_url 'https://github.com/sous-chefs/ruby_rbenv'
+issues_url 'https://github.com/sous-chefs/ruby_rbenv/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/sous-chefs/ruby_rbenv' if respond_to?(:source_url)
 license 'Apache 2.0'
 description 'Manages rbenv and its installed rubies. Several LWRPs are also defined.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.1.0'
-chef_version '>= 12.0'
+chef_version '>= 12.0' if respond_to?(:chef_version)
 
 depends 'ruby_build' # if using the rbenv LWRP, ruby-build must be installed
 recommends 'java', '> 1.4.0' # if using jruby, java is required on system
