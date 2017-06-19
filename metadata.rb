c08a875f69cf89121387557ec783eb929dca356b
Anton Rieder <aried3r@gmail.com>
2017-06-19 16:04:32 +0200

    Fix some Foodcritic issues

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
 issues_url 'https://github.com/sous-chefs/ruby_rbenv/issues'
 source_url 'https://github.com/sous-chefs/ruby_rbenv'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Manages rbenv and its installed rubies. Several LWRPs are also defined.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.2.0'
