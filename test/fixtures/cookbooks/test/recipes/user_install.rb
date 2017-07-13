d7259b1aa13d1fc44af91dcd5fdab097459c43a2
Dan Webb <dan.webb@damacus.io>
2017-07-13 16:03:11 +0100

    Fix user install resource for dokken

--- test/fixtures/cookbooks/test/recipes/user_install.rb
+++ test/fixtures/cookbooks/test/recipes/user_install.rb
@@ -1,4 +1,12 @@
 # Install rbenv and makes it avilable to the selected user
+
+# Make sure Vagrant user is on the box. This should fix the dokken user install
+user 'vagrant'
+
+group 'vagrant' do
+  members 'vagrant'
+end
+
 # Keeps the rbenv install upto date
 rbenv_user_install 'vagrant' do
   update_rbenv true
