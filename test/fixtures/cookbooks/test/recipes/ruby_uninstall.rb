7bd25dc5b5acce96382841af6514e449e84710e4
Anton Rieder <aried3r@gmail.com>
2018-08-21 15:35:15 +0200

    Don't mix system wide and user installs

--- test/fixtures/cookbooks/test/recipes/ruby_uninstall.rb
+++ test/fixtures/cookbooks/test/recipes/ruby_uninstall.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 system_version = '2.5.1'
-user_version = '2.5.0'
 
 # Make sure that Vagarant user is on the box for dokken
 include_recipe 'test::dokken'
@@ -15,10 +14,7 @@ rbenv_ruby system_version
 # Set System global version
 rbenv_global system_version
 
-# User Install
-rbenv_user_install 'vagrant'
-
-# Install a Ruby to a user directory
-rbenv_ruby user_version do
-  user 'vagrant'
+# Uninstall Ruby
+rbenv_ruby system_version do
+  rbenv_action 'uninstall'
 end
