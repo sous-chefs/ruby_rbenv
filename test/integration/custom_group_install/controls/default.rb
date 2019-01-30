d94f0ad80490c19d5d71c204e8cadc44e3f8df27
Dan Webb <dan.webb@damacus.io>
2019-01-30 10:32:52 +0000

    Add custom group test
    - Update CircleCI orb to 1.0.1
    - Remove Gemfile

---
+++ test/integration/custom_group_install/controls/default.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+global_ruby = '2.4.1'
+
+control 'Rbenv should be installed' do
+  title 'Rbenv should be installed to the users home directory'
+
+  desc 'Rbenv should be installed'
+  describe bash('sudo -H -u vagrant bash -c "source /etc/profile.d/rbenv.sh && rbenv global"') do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should include(global_ruby) }
+  end
+
+  describe file('/home/vagrant/.rbenv/versions') do
+    it { should exist }
+    it { should be_writable.by_user('vagrant') }
+    its('group') { should eq 'new-group' }
+  end
+
+  describe file('/home/vagrant/.rbenv') do
+    it { should exist }
+    it { should be_writable.by_user('vagrant') }
+    its('group') { should eq 'new-group' }
+  end
+end
+
+control 'ruby-build plugin should be installed' do
+  title 'ruby-build should be installed to the users home directory'
+  describe bash('sudo -H -u vagrant bash -c "source /etc/profile.d/rbenv.sh && rbenv install -l"') do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should include('2.3.4') }
+    its('stdout') { should include(global_ruby) }
+  end
+end
+
+control 'Global Ruby' do
+  title 'Rbenv should be installed globally'
+
+  desc "Can set global Ruby version to #{global_ruby}"
+  describe bash('sudo -H -u vagrant bash -c "source /etc/profile.d/rbenv.sh && rbenv versions --bare"') do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should include(global_ruby) }
+  end
+end
