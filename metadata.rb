dae98c223c7367a7edf2eba2350c29ad17a35243
Tim Smith <tsmith@chef.io>
2019-10-24 18:42:19 -0700

    Remove the unnecessary long_metadata value in metadata.rb (#267)
    No need for this anymore
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,6 @@ issues_url 'https://github.com/sous-chefs/ruby_rbenv/issues'
 source_url 'https://github.com/sous-chefs/ruby_rbenv'
 license 'Apache-2.0'
 description 'Manages rbenv and installs Rbenv based Rubies'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.3.0'
 chef_version '>= 13.0'
 
