a762403525aff2978bf040491ce2076c54997592
Dan Webb <dan.webb@damacus.io>
2026-03-17 17:15:51 +0000

    fix: make rbenv profile script robust against rbenv init failures
    - Add shims dir to PATH so Ruby commands work even if rbenv init fails
    
    - Suppress rbenv init stderr and exit code to prevent source failures
    
    - Add diagnostic InSpec control to capture environment state on CI

--- test/integration/user_install/controls/user_install.rb
+++ test/integration/user_install/controls/user_install.rb
@@ -2,6 +2,26 @@
 
 global_ruby = '3.4.9'
 
+control 'rbenv environment diagnostic' do
+  title 'Verify rbenv environment is correctly configured'
+
+  describe bash('getent passwd vagrant') do
+    its('stdout') { should include '/home/vagrant' }
+  end
+
+  describe bash('sudo -H -u vagrant bash -c "echo HOME=\$HOME"') do
+    its('stdout') { should include 'HOME=/home/vagrant' }
+  end
+
+  describe bash('sudo -H -u vagrant bash -c "test -d \$HOME/.rbenv && echo DIR_EXISTS || echo DIR_MISSING"') do
+    its('stdout') { should include 'DIR_EXISTS' }
+  end
+
+  describe bash('sudo -H -u vagrant bash -c "source /etc/profile.d/rbenv.sh 2>&1; echo RBENV_ROOT=\$RBENV_ROOT; which rbenv 2>&1 || true"') do
+    its('stdout') { should include 'RBENV_ROOT=/home/vagrant/.rbenv' }
+  end
+end
+
 control 'Rbenv should be installed' do
   title 'Rbenv should be installed to the users home directory'
 
